e8d7eeaba7c397ada97646405a0ac8edab31e669
ikerserna <iker@chumeus.com>
2024-06-19 09:27:34 +0200

    Progress on the website (configurations with the database and configurations of migrations).

---
+++ SistemaInventario.Modelos/Store.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser máxmio 60 Caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripción es Requerido")]
+        [MaxLength(60, ErrorMessage = "Nombre debe ser máxmio 100 Caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage ="El estado es Requerido")]
+        public bool Estado { get; set; }
+    }
+}
