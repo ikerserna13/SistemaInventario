d1dd046fb92a1d68984cdc6617c6bfc278f1debd
ikerserna <iker@chumeus.com>
2024-06-25 09:12:58 +0200

    Brand CRUD

---
+++ SistemaInventario.AccesoDatos/Migrations/20240625070816_AgregarMarcaMigracion2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarMarcaMigracion2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descripcion",
+                table: "Marcas",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descripcion",
+                table: "Marcas");
+        }
+    }
+}
